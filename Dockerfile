b3027420816bf5ccacd20f8d385d44eff5682a11
Ljubo Nikolic <ljubonikolic@gmail.com>
2021-11-16 00:15:32 +0100

    WIP - fixing permissions for waltz user and running tomcat

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,27 @@
-FROM tomcat:8-jdk8-openjdk
+FROM tomcat:8-jre8-temurin
 
-ENV PATH="/bin/liquibase:${PATH}" 
-ENV LIQUIBASE_HOME="/bin/liquibase"
+ENV PATH="/usr/local/bin/liquibase:${PATH}" 
 
-RUN mkdir -p /root/.waltz && \
-  wget -q https://github.com/liquibase/liquibase-package-manager/releases/download/v0.1.2/lpm-0.1.2-linux.zip && \
-  wget -q https://github.com/liquibase/liquibase/releases/download/v4.5.0/liquibase-4.5.0.zip && \
-  unzip -qo lpm-0.1.2-linux.zip -d /bin && \
-  unzip -qo liquibase-4.5.0.zip -d /bin/liquibase && \
-  apt-get update && apt-get install -y postgresql-client gettext-base && \
+RUN useradd -ms /bin/bash waltz && \
+  chown -R waltz /usr/local/tomcat && \
+  mkdir -p /home/waltz/.waltz && \
+  curl -sLO https://github.com/liquibase/liquibase-package-manager/releases/download/v0.1.2/lpm-0.1.2-linux.zip && \
+  curl -sLO https://github.com/liquibase/liquibase/releases/download/v4.5.0/liquibase-4.5.0.zip && \
+  apt-get update && apt-get install -y unzip postgresql-client gettext-base && \
+  unzip -qo lpm-0.1.2-linux.zip -d /usr/local/bin && \
+  unzip -qo liquibase-4.5.0.zip -d /usr/local/bin/liquibase && \
   rm -rf /var/lib/apt/lists/* lpm-0.1.2-linux.zip liquibase-4.5.0.zip && \
   lpm update && lpm add -g postgresql
 
-COPY docker/docker-entrypoint.sh /usr/local/tomcat/docker-entrypoint.sh
-COPY waltz-data/src/main/ddl/liquibase/db.changelog-master.xml /usr/local/tomcat/db.changelog-master.xml
+COPY docker/docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+COPY ./waltz-data/src/main/ddl/liquibase/*.xml /opt/waltz/liquibase/
 COPY ./waltz-web/target/waltz-web.war /usr/local/tomcat/webapps
-COPY docker/waltz.properties /root/.waltz/waltz-template.properties
-COPY waltz-web/src/main/resources/logback.example.xml /root/.waltz/waltz-logback.xml
+COPY docker/waltz.properties /home/waltz/.waltz/waltz-template.properties
+COPY waltz-web/src/main/resources/logback.example.xml /home/waltz/.waltz/waltz-logback.xml
 
 EXPOSE 8080
 
-ENTRYPOINT [ "./docker-entrypoint.sh" ]
+USER waltz
+
+ENTRYPOINT [ "/usr/local/bin/docker-entrypoint.sh" ]
 CMD [ "update",  "run" ] 
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+check=( DB_HOST DB_PORT DB_NAME DB_USER DB_PASSWORD DB_SCHEME WALTZ_FROM_EMAIL WALTZ_BASE_URL CHANGELOG_FILE )
+for x in "${check[@]}"
+do
+  if [ -z "$x" ]; then 
+    echo "$x not provided"
+    case $x in
+      DB_HOST)
+        export DB_HOST="postgres"
+        ;;
+      DB_PORT)
+        export DB_PORT="5432"
+        ;;
+      DB_NAME | DB_USER | DB_PASSWORD | DB_SCHEME)
+        export $x="waltz"
+        ;;
+      WALTZ_FROM_EMAIL)
+        export WALTZ_FROM_EMAIL="help@finos.org"
+        ;;
+      WALTZ_BASE_URL)
+        export WALTZ_BASE_URL="http://127.0.0.1:8080/"
+        ;;
+      CHANGELOG_FILE)
+        export CHANGELOG_FILE="/opt/waltz/liquibase/db.changelog-master.xml"
+        ;;
+    esac
+  fi
+done
+
+
 db_action () {
   while [[ $(pg_isready -U "${DB_USER}" -h "${DB_HOST}" -d "${DB_NAME}") == *"no response"* ]]
   do
@@ -12,33 +41,26 @@ db_action () {
   # changeLogFile must be relative path
   DB_UPDATE=$(liquibase --changeLogFile=../../../${CHANGELOG_FILE} --hub-mode=off --username="${DB_USER}" --password="${DB_PASSWORD}" --url=jdbc:postgresql://"${DB_HOST}":"${DB_PORT}"/"${DB_NAME}" "$1")
 
-  if $DB_UPDATE; then
-    echo "Success"
-  else
-    echo "Liquibase update failed"
-    exit 1
-  fi
-  
+  $DB_UPDATE
+
+  # TODO - find better way to confirm executation of liquibase
+  # sleep 9999
+  # $DB_UPDATE 2> /dev/null || error_code=$?
+  # if [[ $error_code -ne "0" ]]; then
+  #   echo "Liquibase update failed"
+  #   exit 1
+  # fi
 }
 
 run_waltz () {
-  envsubst < /root/.waltz/waltz-template.properties > /root/.waltz/waltz.properties
+  envsubst < /home/waltz/.waltz/waltz-template.properties > /home/waltz/.waltz/waltz.properties
   catalina.sh run
 }
 
-DEFAULT_DB_COMMAND="update"
-DB_COMMAND="${1:-$DEFAULT_DB_COMMAND}"
-DEFAULT_WALTZ_COMMAND="run"
-WALTZ_COMMAND="${1:-$DEFAULT_WALTZ_COMMAND}"
-
-if [[ $DB_COMMAND == "$DEFAULT_DB_COMMAND" && -z $2 ]]; then
-  echo ">>> Update DB"
-  db_action "$1"
-elif  [[ $WALTZ_COMMAND == "$DEFAULT_WALTZ_COMMAND" && -z $2 ]]; then
-  echo ">>> Run Waltz"
-  run_waltz
-else
-  echo ">>> Update DB and Run Waltz"
-  db_action update
-  run_waltz
-fi
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        update) echo ">>> Update DB" && db_action update ;;
+        run) echo ">>> Run Waltz" && run_waltz ;;
+    esac
+    shift
+done
